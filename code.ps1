8a113d73e2e238ff54cfb9090e18725634943454
chaitanya chowdary <56665907+ChaitanyaChandra@users.noreply.github.com>
2021-11-12 23:02:59 +0530

    Create code.ps1

---
+++ code.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install git -y
+choco install virtualbox -y
+choco install ruby -y
+choco install packer -y
+choco install vagrant -y
+choco install microsoft-edge -y
