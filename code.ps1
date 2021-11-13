8f4fa967a4ae9f51fcc0228b5d75f32f0c0a88d3
chaitanya chowdary <56665907+ChaitanyaChandra@users.noreply.github.com>
2021-11-13 16:11:50 +0530

    Update code.ps1

--- code.ps1
+++ code.ps1
@@ -1,6 +1,28 @@
+# install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# install git
 choco install git -y
-choco install ruby -y
-choco install packer -y
-choco install vagrant -y
+
+# install microsoft edge
 choco install microsoft-edge -y
+
+# install ssh client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# install ssh server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
