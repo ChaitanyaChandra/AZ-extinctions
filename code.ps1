8a9248525c1975c546660628479533176e69c994
chaitanya chowdary <56665907+ChaitanyaChandra@users.noreply.github.com>
2021-11-16 21:50:18 +0530

    Update code.ps1

--- code.ps1
+++ code.ps1
@@ -11,11 +11,10 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 # Start the sshd service
-Start-Service sshd, ssh-agent
+Start-Service sshd
 
 # OPTIONAL but recommended:
 Set-Service -Name sshd -StartupType 'Automatic'
-Set-Service -Name ssh-agent -StartupType 'Automatic'
 
 # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
